95e1c7efadf18118e4666bb961d270f2736454b7
Cor Hortensius <cor.hortensius@kentivo.com>
2021-04-06 10:59:28 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div><h1>This is a message from a Static Webapp Running on Azure, and deployed with Actions</h1><br>Hello {value}</div>;
 }
 
 export default App;
